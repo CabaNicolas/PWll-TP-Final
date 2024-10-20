0ff330c9712e4c8dcd2966c8f61655379f0cb041
Gabriela <leverattomariag@gmail.com>
2024-10-20 19:05:00 -0300

    Se permite cambiar la imagen de perfil y se elimina la anterior del directorio. Agregando metodos al usuarioModel

--- app/model/UsuarioModel.php
+++ app/model/UsuarioModel.php
@@ -158,23 +158,34 @@ class UsuarioModel
         where mail = '" . $mail . "'";
         $usuario = $this->database->query($sql);
 
-        //var_dump($usuario);
-        
+        //
+        var_dump($usuario);
+        //
+
         return $usuario;
     }
 
     public function actualizarDatosPerfil($nombreUsuario, $mail, $nombreCompleto, $fechaNacimiento, $sexo, $foto, $password) {
 
         $mailActual = $_SESSION['mail'];
+        $sqlImagenActual = "SELECT foto FROM USUARIO WHERE mail = '" . $mailActual . "'";
+        $imagenActual = $this->database->query($sqlImagenActual);
+
+
+        $nombreImagenActual = !empty($imagenActual) ? $imagenActual[0]['foto'] : null;
 
         $sql = "UPDATE usuario 
-                SET nombreUsuario = '" . $nombreUsuario . "',
-                mail = '" . $mail . "', 
-                nombreCompleto = '". $nombreCompleto ."',
-                fechaNacimiento = '" . $fechaNacimiento . "', 
-                idSexo = (SELECT id FROM sexo WHERE nombre LIKE '%" . $sexo . "%')";
+        SET nombreUsuario = '" . $nombreUsuario . "',
+        mail = '" . $mail . "', 
+        nombreCompleto = '" . $nombreCompleto . "',
+        fechaNacimiento = '" . $fechaNacimiento . "', 
+        idSexo = (SELECT id FROM sexo WHERE nombre LIKE '%" . $sexo . "%')";
 
 
+        $nuevoNombreImagen = $this->manejarFoto($foto, $nombreImagenActual);
+        if ($nuevoNombreImagen) {
+            $sql .= ", foto = '" . $nuevoNombreImagen . "'";
+        }
 
         if (!empty($password)) {
             $sql .= ", password = '" . $password . "'";
@@ -182,20 +193,40 @@ class UsuarioModel
 
         $sql .= " WHERE mail = '" . $mailActual . "'";
 
-        $cambio=$this->database->add($sql);
+        $cambio = $this->database->add($sql);
 
-        if ($cambio == 1) {
-            $cambio =[
-                "exito" => true,
-                "mensaje" => "Cambios realizados correctamente"
-            ];
-        } else{
-            $cambio = [
-                "exito" => false,
-                "mensaje" => "Los cambios no se pudieron realizar"
-            ];
-        }
-        return $cambio;
+        return [
+            "exito" => $cambio == 1,
+            "mensaje" => $cambio == 1 ? "Cambios realizados correctamente" : "Los cambios no se pudieron realizar"
+        ];
     }
+
+    private function manejarFoto($foto, $nombreImagenActual) {
+        if (!empty($foto['name'])) {
+            $targetDir = "images/";
+            $targetFile = basename($foto["name"]);
+
+
+            if (move_uploaded_file($foto["tmp_name"], $targetDir . $targetFile)) {
+
+                if (!empty($nombreImagenActual) && file_exists($targetDir . $nombreImagenActual)) {
+                    $this->deleteImagen($nombreImagenActual);
+                }
+                return $targetFile;
+            }
+        }
+        return null;
+    }
+
+    private function deleteImagen($nombreImagen) {
+
+        $rutaImagenCompleta = $_SERVER['DOCUMENT_ROOT'] . '/images/' . $nombreImagen; 
+
+        if (file_exists($rutaImagenCompleta)) {
+            unlink($rutaImagenCompleta);
+        }
+    }
+
+
 
 }
