269bc451805d5a531fe7b1a7957a4ebe96fb7335
NadiaSpi <nadia.belen98@gmail.com>
2024-11-11 12:51:27 -0300

    BDD actualizada con Rol editor

--- pw2.sql
+++ pw2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-11-2024 a las 13:30:13
+-- Tiempo de generación: 11-11-2024 a las 16:49:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -73,7 +73,9 @@ INSERT INTO `partida` (`idPartida`, `idUsuario`, `fecha`, `puntaje`, `preguntaAc
 (7, 3, '2024-11-10', 0, 0, 1),
 (8, 3, '2024-11-10', 1, 4, 1),
 (9, 3, '2024-11-10', 0, 7, 1),
-(10, 3, '2024-11-10', 2, 11, 1);
+(10, 3, '2024-11-10', 2, 11, 1),
+(11, 2, '2024-11-11', 1, 1, 1),
+(12, 2, '2024-11-11', 0, 27, 1);
 
 -- --------------------------------------------------------
 
@@ -105,7 +107,7 @@ INSERT INTO `pregunta` (`idPregunta`, `descripcion`, `categoria`, `correcto`, `i
 (8, '¿En qué año comenzó la Segunda Guerra Mundial?', 3, 4, 0, 'activa'),
 (9, '¿Cuántos jugadores tiene un equipo de fútbol en el campo?', 4, 4, 0, 'activa'),
 (10, '¿En qué deporte se utiliza una pelota de 3 agujeros?', 4, 2, 3, 'activa'),
-(11, '¿Quién pintó la Mona Lisa?', 5, 5, 0, 'activa'),
+(11, '¿Quién pintó la Mona Lisa?', 5, 6, 0, 'activa'),
 (12, '¿A qué movimiento artístico pertenece el cuadro \"La noche estrellada\"?', 5, 3, 3, 'activa'),
 (27, 'En que parte se encuentra el obelisco', 2, 5, 1, 'activa');
 
@@ -139,15 +141,17 @@ CREATE TABLE `reportes_preguntas` (
   `idReporte` int(11) NOT NULL,
   `idPregunta` int(11) NOT NULL,
   `idUsuario` int(11) NOT NULL,
-  `motivo` text NOT NULL
+  `motivo` text NOT NULL,
+  `estado` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `reportes_preguntas`
 --
 
-INSERT INTO `reportes_preguntas` (`idReporte`, `idPregunta`, `idUsuario`, `motivo`) VALUES
-(2, 11, 3, 'ver si pierdo la partida\r\n');
+INSERT INTO `reportes_preguntas` (`idReporte`, `idPregunta`, `idUsuario`, `motivo`, `estado`) VALUES
+(3, 1, 2, 'La descripcion esta mal', 'rechazada'),
+(4, 27, 2, 'La descripcion esta mal escrita', 'pendiente');
 
 -- --------------------------------------------------------
 
@@ -174,7 +178,10 @@ INSERT INTO `responde` (`idUsuario`, `idPregunta`) VALUES
 (3, 7),
 (3, 8),
 (3, 2),
-(3, 11);
+(3, 11),
+(2, 11),
+(2, 1),
+(2, 27);
 
 -- --------------------------------------------------------
 
@@ -336,9 +343,10 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id`, `mail`, `password`, `nombreUsuario`, `fechaNacimiento`, `nombreCompleto`, `foto`, `idSexo`, `token_verificacion`, `cuenta_verificada`, `rol_fk`) VALUES
 (1, 'nico@caba.com', '$2y$10$a6xiV/zP6M162QNYLMNhz.zaWbcw/MxnfqMMueYsBcNCBm8C7mJwq', 'BigPaik', '1997-11-27', 'Nicolas Caba', 'Galactus.webp', 1, NULL, 'A', 2),
-(2, 'nadia@gmail.com', '$2y$10$K42yPSwoNqHkuHfkZ591ReMlG9L1.YRdFV.paZ3.4AclCgtnXZ6yC', 'Nadi', '2000-08-09', 'Nadia Spinelli', 'avatar.jpg', 2, NULL, 'A', 0),
+(2, 'nadia@gmail.com', '$2y$10$K42yPSwoNqHkuHfkZ591ReMlG9L1.YRdFV.paZ3.4AclCgtnXZ6yC', 'Nadi', '2000-08-09', 'Nadia Spinelli', 'avatar.jpg', 2, NULL, 'A', 3),
 (3, 'leverattomariag@gmail.com', '$2y$10$yTJFgcwQRz.QEjEX7WmIAO0sPEfy/AaTOBFT4Z1s5r8b92a7g9/wy', 'gabi', '2000-02-20', 'Maria Gabriela Leveratto', 'avatar.jpg', 1, NULL, 'A', 3),
-(4, 'admin@preguntones.com', '$2y$10$/MlVhHRMNPUmOv7YhIwarOjE./iist0xGU1JbRr3eC/hqEv9iCY.q', '111111', '2000-02-20', 'Admin', 'avatar.jpg', 3, NULL, 'A', 1);
+(4, 'admin@preguntones.com', '$2y$10$/MlVhHRMNPUmOv7YhIwarOjE./iist0xGU1JbRr3eC/hqEv9iCY.q', '111111', '2000-02-20', 'Admin', 'avatar.jpg', 3, NULL, 'A', 1),
+(5, 'editor@gmail.com', '$2y$10$BGjKYi2Wr.Wi.mutabU0HuyjjjIeqzWMC1R2n6g.TfxqVz1VVo3U.', 'Editor', '2000-02-20', 'Editor', 'avatar.jpg', 3, NULL, 'A', 2);
 
 --
 -- Índices para tablas volcadas
@@ -419,7 +427,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `partida`
 --
 ALTER TABLE `partida`
-  MODIFY `idPartida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idPartida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `pregunta`
@@ -437,7 +445,7 @@ ALTER TABLE `pregunta_sugerida`
 -- AUTO_INCREMENT de la tabla `reportes_preguntas`
 --
 ALTER TABLE `reportes_preguntas`
-  MODIFY `idReporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idReporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `respuesta`
@@ -467,7 +475,7 @@ ALTER TABLE `sexo`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
